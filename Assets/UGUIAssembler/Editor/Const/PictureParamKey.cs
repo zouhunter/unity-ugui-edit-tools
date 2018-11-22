e3f995dc613c87fcdd1362a3e891f3fcc7ac2569
邹杭特 <zouhunter@126.com>
2018-11-22 21:52:39 +0800

    更新配制设定

---
+++ Assets/UGUIAssembler/Editor/Const/PictureParamKey.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGUIAssembler
+{
+    public static class PictureParamKey
+    {
+        public const string sprite_border = "border";
+    }
+}
--- Assets/UGUIAssembler/Editor/UGUIAssemblerEditor.cs
+++ Assets/UGUIAssembler/Editor/UGUIAssemblerEditor.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEditor;
 namespace UGUIAssembler
 {
+    /// <summary>
+    /// 1.从UI导出布局配制表
+    /// 2.通过布局配制表生成UI
+    /// 3.指导指定图片导入格式及信息
+    /// 4.优选设置功能
+    /// </summary>
     public class UGUIAssemblerEditor : Editor
     {
 
