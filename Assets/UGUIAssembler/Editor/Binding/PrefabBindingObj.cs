7b095330590b33b8aa34871b070270c4ecff5b16
邹杭特 <zouhunter@126.com>
2018-11-23 11:56:13 +0800

    更改数据的解析方式

---
+++ Assets/UGUIAssembler/Editor/Binding/PrefabBindingObj.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+namespace UGUIAssembler
+{
+    public class PrefabBindingObj : ScriptableObject
+    {
+        public enum NodeType
+        {
+            single, multiple, quote
+        }
+        [System.Serializable]
+        public class ValueNode
+        {
+            public string name;
+            public NodeType type;
+            public string text;
+
+            public Vector3 vector3Value
+            {
+                get
+                {
+                   return ParamAnalysisTool.ToVector3(text);
+                }
+                set
+                {
+                    text = ParamAnalysisTool.Vector3ToString(value);
+                }
+            }
+            public int indexValue
+            {
+                get
+                {
+                    return ParamAnalysisTool.ToInit(text);
+                }
+                set
+                {
+                    text = ParamAnalysisTool.IntToString(value);
+                }
+            }
+        }
+
+        [System.Serializable]
+        public class ScriptItem
+        {
+            public string name;
+            public int index;
+            public string path;
+            public string scriptName;
+            public string assemble;
+            public List<ValueNode> nodes;
+        }
+
+        public List<ScriptItem> bindingItems = new List<ScriptItem>();
+    }
+}
--- Assets/UGUIAssembler/Editor/Util/ParamAnalysisTool.cs
+++ Assets/UGUIAssembler/Editor/Util/ParamAnalysisTool.cs
@@ -1,10 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace UGUIAssembler
 {
     public class ParamAnalysisTool
     {
+        public const char seperator = '·';
 
+        public static Vector3 ToVector3(string param)
+        {
+            if (string.IsNullOrEmpty(param)) return Vector3.zero;
+
+            string[] split = param.Split(seperator);
+            var value = Vector3.zero;
+            for (int i = 0; i < split.Length; i++)
+            {
+                if (i < 3)
+                {
+                    float fvalue = 0;
+                    if (float.TryParse(split[i], out fvalue))
+                    {
+                        value[i] = fvalue;
+                    }
+                }
+            }
+            return value;
+        }
+
+        public static string Vector3ToString(Vector3 value)
+        {
+            var array = new string[3];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value[i].ToString("0.00");
+            }
+            return string.Join(seperator.ToString(), array);
+        }
+
+        public static string IntToString(int value)
+        {
+            return value.ToString();
+        }
+
+        public static int ToInit(string text)
+        {
+            var intValue = 0;
+            int.TryParse(text, out intValue);
+            return intValue;
+        }
     }
 }
